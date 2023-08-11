b6b6578ef054fd1e7692ee832d8dba064b924f82
Trevor Machete <trevormachete@gmail.com>
2023-08-11 03:14:25 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,7 @@
-#!/bin/bash
-echo '#include <stdio.h> int main(void){    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);}' > art.c
+#include <stdio.h>
 
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
