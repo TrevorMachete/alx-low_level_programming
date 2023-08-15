d331b0f06caa0a08fb801325142b0ff3dc66abd7
Trevor Machete <trevormachete@gmail.com>
2023-08-15 19:29:16 +0200

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,11 +10,11 @@ int main(void)
 int i;
 unsigned long f1 = 0, f2 = 1, next;
 
-for (i = 1; i <= 98; i++)
+for (i = 1; i <= 95; i++)
 {
 next = f1 + f2;
 printf("%lu", next);
-if (i != 98)
+if (i != 95)
 printf(", ");
 f1 = f2;
 f2 = next;
