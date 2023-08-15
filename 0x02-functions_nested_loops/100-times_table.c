edd21a8fb40ab2ad8020451ca42e5e398aaad74a
Trevor Machete <trevormachete@gmail.com>
2023-08-15 18:14:36 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,7 +14,7 @@ if (n < 0 || n > 15)
 return;
 
 for (i = 0; i <= n; i++)
-    {
+{
 for (j = 0; j <= n; j++)
 {
 product = i * j;
@@ -32,9 +32,9 @@ if (product >= 100)
 _putchar(product / 100 + '0');
 _putchar((product / 10) % 10 + '0');
 _putchar(product % 10 + '0');
-            }
+}
 else if (product >= 10)
-            {
+{
 _putchar(product / 10 + '0');
 _putchar(product % 10 + '0');
 }
