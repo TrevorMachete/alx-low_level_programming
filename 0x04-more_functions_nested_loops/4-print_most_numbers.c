659ae15c5505289967d60e56da4fd14499d82f5b
Trevor Machete <trevormachete@gmail.com>
2023-08-18 06:18:32 +0200

    Update and rename 4-print_most_numbers to 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,16 @@
 
 void print_most_numbers(void)
 {
-	char n = 0;
+char n = 0;
 
-	while (n <= 9)
-	{
-		if (n != 2 && n != 4)
-		{
-			_putchar('0' + n);
-		}
+while (n <= 9)
+{
+if (n != 2 && n != 4)
+{
+_putchar('0' + n);
+}
 
-		n++;
-	}
-	_putchar('\n');
+n++;
+}
+_putchar('\n');
 }
