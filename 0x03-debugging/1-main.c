ae0ab9715a5d9e382ca93a4da1313d212400cd29
Trevor Machete <trevormachete@gmail.com>
2023-08-18 08:23:13 +0200

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,26 +1,24 @@
-#include <stdlib.h>
-#include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
 int main(void)
 {
-int n;
+int i;
 
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
+printf("Infinite loop incoming :(\n");
+
+i = 0;
 /*
-while (n > 0)
-{
-if (n > 0 && n <= 10)
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
-else if (n > 5)
-printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-else if (n == 0)
-printf("Last digit of %d is %d and is 0\n", n, n % 10);
-n /= 10;
-}
+*while (i < 10)
+*{
+*		putchar(i);
+*}
 */
+printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
