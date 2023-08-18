9a247ade0ce29a091b75eed03c5ed190a298593d
Trevor Machete <trevormachete@gmail.com>
2023-08-18 06:48:21 +0200

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,26 @@
-Trevormachete@outlook.com #include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
 
-/**
-* main - Test function for positive or negative
-* Return: 0
-*/
-
+/* betty style doc for function main goes there */
 int main(void)
 {
-int i;
+int n;
 
-i = 0;
-positive_or_negative(i);
-
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+/*
+while (n > 0)
+{
+if (n > 0 && n <= 10)
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+else if (n > 5)
+printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+else if (n == 0)
+printf("Last digit of %d is %d and is 0\n", n, n % 10);
+n /= 10;
+}
+*/
 return (0);
 }
