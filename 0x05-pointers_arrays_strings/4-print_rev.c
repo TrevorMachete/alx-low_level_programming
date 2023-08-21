bfed1187f2df2059553098a3aefc062a6cc2f65c
Trevor Machete <trevormachete@gmail.com>
2023-08-22 00:52:20 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,16 +7,12 @@
 
 void print_rev(char *s)
 {
-	int i = 0;
-
-	while (s[i])
-		i++;
-
-	while (i--)
-	{
-		_putchar(s[i]);
-	}
-
-	_putchar('\n');
-
+int i = 0;
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
 }
